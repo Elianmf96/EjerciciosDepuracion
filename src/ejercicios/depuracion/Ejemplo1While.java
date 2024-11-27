db969f2b047769d23822cb85e2e70d38fc0c685c
elianmf <elianmf96@educastur.es>
2024-11-27 17:54:37 +0100

    atributos y constructor

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -2,15 +2,22 @@ package ejercicios.depuracion;
 
 import java.util.Scanner;
 public class Ejemplo1While {
+    private int suma, num;
+
+    public Ejemplo1While() {
+	this.suma = 0;
+	this.num = 0;
+    }
+
     public static void main(String[] args) {
         int suma = 0, num;
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca un número: ");
         num = sc.nextInt();
         while (num>= 0){
-               suma = suma + num;
-               System.out.print("Introduzca un número: ");
-               num = sc.nextInt();
+            suma = suma + num;
+            System.out.print("Introduzca un número: ");
+            num = sc.nextInt();
         }
         System.out.println("La suma es: " + suma );
     }
